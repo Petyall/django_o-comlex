36467e9c7af5616eaebe5c43d24bcaa5c7be8b1e
petyal <alekolen35@gmail.com>
2024-07-16 23:24:00 +0300

    open meteo api requests added

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from weather.utils import get_weather, get_coordinates
+
 
 def main(request):
-    return render(request, 'weather/main.html')
+    weather_data = None
+    if 'city' in request.GET:
+        city = request.GET['city']
+        latitude, longitude, message = get_coordinates(city)
+        if latitude and longitude:
+            print(latitude, longitude)
+            weather_data = get_weather(latitude, longitude)
+            weather_data['city'] = city
+        if message:
+            message_data = {'message': message}
+            return render(request, 'weather/main.html', {'weather_data': weather_data, 'message_data': message_data})
+    return render(request, 'weather/main.html', {'weather_data': weather_data})
